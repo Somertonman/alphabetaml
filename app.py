3430703236b361a4befca8c2638cfed1cf9498cf
Somertonman <8521878+Somertonman@users.noreply.github.com>
2021-11-28 18:51:51 +0400

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ file = st.file_uploader("Please upload an image file or...", type=["csv", "txt"]
 
 if file:
 	df = pd.read_csv(file)
-	st.write(df)
+	st.write(df.head())
+
+
+if file:
+	ab_column = st.sidebar.selectbox("Resulting test/control group", df.columns)
